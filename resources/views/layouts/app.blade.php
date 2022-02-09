98b858010fad813b31ec7c6fa2398b398476af2f
govind <87171832+govind7773@users.noreply.github.com>
2022-02-09 18:56:08 +0530

    making responsive app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <title>{{ config('app.name', 'ETShare') }}</title>
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
@@ -17,50 +18,21 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <style>@media screen and (max-width: 500px) {
+        #left_menu{
+                display:none;
+            }
+    }</style>
 </head>
 <body >
-<div class="container-fluid">
-    <div class="row flex-nowrap">
-        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-            <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-                <a href="/" class="d-flex align-items-center mb-md-0 me-md-auto text-white text-decoration-none"><img src="{{asset('logo.png')}}" alt=""  width="60" height="60" class="rounded-circle ml-3">
-                </a>
-                <a href="/" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-                    <span class="fs-4 d-none d-sm-inline mx-2">ETShare</span>
-                </a>
-                <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-                    <li class="nav-item">
-                        <a href="#" class="nav-link align-middle px-0">
-                        <i class="fas fa-home"></i><span class="ms-1 d-none d-sm-inline">Home</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#submenu3" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
-                            <i class="fa fa-grid"></i> <span class="ms-1 d-none d-sm-inline">Products</span> </a>
-                            <ul class="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
-                            <li class="w-100">
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 1</a>
-                            </li>
-                            <li>
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 2</a>
-                            </li>
-                            <li>
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 3</a>
-                            </li>
-                            <li>
-                                <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 4</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="#" class="nav-link px-0 align-middle">
-                            <i class="fa fa-people"></i> <span class="ms-1 d-none d-sm-inline">Customers</span> </a>
-                    </li>
-                </ul>
-                <hr>
-                <div class="dropdown pb-4">
+<nav class="navbar-expand-sm bg-dark navbar navbar-dark sticky-top navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand fs-4 py-0 px-3" href="#">ETShare <i class="fa-solid fa-bars text-white pl-3" id="bar_button"></i></a>
+        </div>
+        <div class="dropdown nav navbar-nav navbar-right">
                     <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                        <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" class="rounded-circle">
+                        <img src="{{ asset('user_logo.png') }}" alt="" width="30" height="30" class="rounded-circle">
                         <span class="d-none d-sm-inline mx-1">Welcome {{ Auth::user()->name }}</span>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-dark  shadow" aria-labelledby="dropdownUser1">
@@ -78,6 +50,46 @@
                         
                     </ul>
                 </div>
+    </div>
+    
+</nav>
+<div class="container-fluid">
+    
+    <div class="row flex-nowrap">
+        <div class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark sticky-left" id="left_menu">
+            <div class="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                <a href="/" class="d-flex align-items-center mb-md-0 me-md-auto text-white text-decoration-none pb-3"><img src="{{asset('logo.png')}}" alt=""  width="60" height="60" class="rounded-circle ml-2">
+                </a>
+                <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link align-middle px-0">
+                        <i class="fas fa-home"></i><span class="ms-1 d-sm-inline">Home</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link align-middle px-0">
+                        <i class="fas fa-plus"></i><span class="ms-1 d-sm-inline">New Panel</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#submenu3" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
+                        <i class="fa fa-list"></i> <span class="ms-1 d-sm-inline">Panels</span> </a>
+                            <ul class="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
+                            <li class="w-100">
+                                <a href="#" class="nav-link px-0"> <span class="d-sm-inline">Product</span> 1</a>
+                            </li>
+                            <li>
+                                <a href="#" class="nav-link px-0"> <span class="d-sm-inline">Product</span> 2</a>
+                            </li>
+                            <li>
+                                <a href="#" class="nav-link px-0"> <span class="d-sm-inline">Product</span> 3</a>
+                            </li>
+                            <li>
+                                <a href="#" class="nav-link px-0"> <span class="d-sm-inline">Product</span> 4</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>  
             </div>
         </div>
         <div class="col py-5 px-5">
@@ -94,4 +106,17 @@
     </div>
 </div>
 </body>
+
+<script>
+    $(document).ready(function(){
+     $('#bar_button').click(function(){
+        if($('#left_menu').css('display') == 'none'){
+            $('#left_menu').fadeIn();
+        }else{
+            $('#left_menu').fadeOut();
+        }
+    }) ;
+});
+   
+</script>
 </html>
